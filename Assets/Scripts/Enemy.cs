41094b37eb263cd3d9a0456d77dfe0897ef64d55
BlazinX <scary_dude_6@hotmail.com>
2018-08-18 11:28:08 +1000

    EnemyAi Added
    EnemySimpleton

---
+++ Assets/Scripts/Enemy.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy : MonoBehaviour
+{
+
+
+    [SerializeField]
+    private Transform[] patrolPoints;
+    [SerializeField]
+    private float speed;
+    Transform currentPatrolPoint;
+    int currentPatrolIndex;
+
+    [SerializeField]
+    private Transform target;
+    [SerializeField]
+    private float chaseRange;
+
+
+    // Use this for initialization
+    void Start()
+    {
+       
+        currentPatrolIndex = 0;
+        currentPatrolPoint = patrolPoints[currentPatrolIndex];
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        enemyChase();
+    }
+
+    void enemyChase()
+    {
+        //Chase Player 
+        //Get the distance to the target and check to see if it is close enough to chase
+        float distanceToTarget = Vector3.Distance(transform.position, target.position);
+        if (distanceToTarget <chaseRange){
+            //Start chasing the target - turn and move towards the target
+            Vector3 targetDir = target.position - transform.position;
+            float angle = Mathf.Atan2(targetDir.y, targetDir.x) * Mathf.Rad2Deg - 90f;
+            Quaternion q = Quaternion.AngleAxis(angle, Vector3.forward);
+            transform.rotation = Quaternion.RotateTowards(transform.rotation, q, 180);
+            transform.Translate(Vector3.up * Time.deltaTime * speed);
+        }
+
+        }
+    
+}
+               
+
+
+
