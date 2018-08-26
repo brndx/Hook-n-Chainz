be9f2f546c419de9bc70d1c56a38f25187993b35
BlazinX <scary_dude_6@hotmail.com>
2018-08-26 21:52:26 +1000

    Added WeaponFiringScript

--- Assets/Scripts/Weapon.cs
+++ Assets/Scripts/Weapon.cs
@@ -5,19 +5,56 @@ using UnityEngine;
 public class Weapon : MonoBehaviour {
 
     [SerializeField] private float fireRate = 0.0f;
-    [SerializeField] private float damage = 10.0f;
-    [SerializeField] private LayerMask notToHit;
+    [SerializeField] private float Damage = 10.0f;
+    [SerializeField] private LayerMask WhatToHit;
 
-   [SerializeField] private float timeToFire = 0.0f;
+    [SerializeField] private float timeToFire = 0.0f;
     private Transform firePoint;
 
-	// Use this for initialization
-	void Awake () {
-        firePoint = transform;
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
+    // Use this for initialization
+    void Awake() {
+        InitializeFirePoint();
+    }
+
+    // Update is called once per frame
+    void Update() {
+        Fire();
+
+    }
+
+    void InitializeFirePoint() {
+        firePoint = transform.Find("FirePoint");
+        if (firePoint == null)
+        {
+            Debug.LogError("No firePoint!");
+        }
+    }
+    void Fire() {
+        if (fireRate == 0)
+        {
+            if (Input.GetButtonDown("Fire1")) {
+                Shoot();
+            }
+        }
+        else
+        {
+            if (Input.GetButton ("Fire1") && Time.time > timeToFire)
+            {
+                timeToFire = Time.time + 1 / fireRate;
+                Shoot();
+            }
+        }    
+    }
+    void Shoot()
+    {
+        Vector2 mousePosition = new Vector2(Camera.main.ScreenToWorldPoint(Input.mousePosition).x, Camera.main.ScreenToWorldPoint(Input.mousePosition).y);
+        Vector2 firePointPosition = new Vector2(firePoint.position.x, firePoint.position.y);
+        RaycastHit2D hit = Physics2D.Raycast(firePointPosition, mousePosition - firePointPosition, 100, WhatToHit);
+        Debug.DrawLine(firePointPosition, (mousePosition-firePointPosition)*100, Color.clear);
+        if (hit.collider != null)
+        {
+            Debug.DrawLine(firePointPosition, hit.point, Color.green);
+            Debug.Log("We hit " + hit.collider.name + " and did " + Damage + " damage.");
+        }
+    }
 }
