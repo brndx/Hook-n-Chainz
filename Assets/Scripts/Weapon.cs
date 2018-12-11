e6cca94acd1e87bb30377dfc42975880871de1c2
BlazinX <scary_dude_6@hotmail.com>
2018-12-11 22:05:50 +1100

    Revert "Revert "Merge branch 'master' of https://github.com/brndx/Hook-n-Chainz""
    This reverts commit f8c2b1d2aabbcd594ece6e1211abad5db02419da.

--- Assets/Scripts/Weapon.cs
+++ Assets/Scripts/Weapon.cs
@@ -4,17 +4,14 @@ using UnityEngine;
 
 public class Weapon : MonoBehaviour {
 
-    [SerializeField] private float fireRate = 0.0f;
     [SerializeField] private float Damage = 10.0f;
-    [SerializeField] private LayerMask WhatToHit;
+    [SerializeField] private Transform Projectile;
+    [SerializeField] private float projectileSpeed = 100.0f;
+    [SerializeField] private float projectileDuration = 2.0f;
+    [SerializeField] private float lastFireTime = 0.0f;
+    [SerializeField] private float fireWait = 0.1f;
 
-    [SerializeField] private float timeToFire = 0.0f;
-    private Transform firePoint;
 
-    // Use this for initialization
-    void Awake() {
-        InitializeFirePoint();
-    }
 
     // Update is called once per frame
     void Update() {
@@ -22,39 +19,26 @@ public class Weapon : MonoBehaviour {
 
     }
 
-    void InitializeFirePoint() {
-        firePoint = transform.Find("FirePoint");
-        if (firePoint == null)
-        {
-            Debug.LogError("No firePoint!");
-        }
-    }
+   
     void Fire() {
-        if (fireRate == 0)
-        {
-            if (Input.GetButtonDown("Fire1")) {
-                Shoot();
-            }
-        }
-        else
-        {
-            if (Input.GetButton ("Fire1") && Time.time > timeToFire)
+            if (Input.GetButton ("Fire1") && Time.time - lastFireTime > fireWait)
             {
-                timeToFire = Time.time + 1 / fireRate;
+                lastFireTime = Time.time;
                 Shoot();
-            }
-        }    
+            } 
     }
     void Shoot()
     {
-        Vector2 mousePosition = new Vector2(Camera.main.ScreenToWorldPoint(Input.mousePosition).x, Camera.main.ScreenToWorldPoint(Input.mousePosition).y);
-        Vector2 firePointPosition = new Vector2(firePoint.position.x, firePoint.position.y);
-        RaycastHit2D hit = Physics2D.Raycast(firePointPosition, mousePosition - firePointPosition, 100, WhatToHit);
-        Debug.DrawLine(firePointPosition, (mousePosition-firePointPosition)*100, Color.clear);
-        if (hit.collider != null)
+        Vector3 mousePosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        Transform clone;
+        clone = Instantiate(Projectile, new Vector3(transform.position.x, 15.0f, transform.position.z), Projectile.rotation);
+        clone.GetComponent<Rigidbody>().velocity = new Vector3(((mousePosition - transform.position).normalized * projectileSpeed).x, 0, ((mousePosition - transform.position).normalized * projectileSpeed).z);
+        Destroy(clone.gameObject, projectileDuration);
+        RaycastHit hit;
+        if (Physics.Raycast(mousePosition, Vector3.down, out hit, Mathf.Infinity))
         {
-            Debug.DrawLine(firePointPosition, hit.point, Color.green);
-            Debug.Log("We hit " + hit.collider.name + " and did " + Damage + " damage.");
+            if (hit.transform.tag == "Enemy")
+            Debug.Log(hit.collider.name);
         }
     }
 }
